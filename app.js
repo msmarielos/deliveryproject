b3e4760b846f83128e65602f7d148ecfc899193f
Ernis <e.badmaev@gmail.com>
2022-01-12 15:41:59 +0300

    rout_registration

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const config = require("./config/config");
+const regRouter = require("./routes/reg.route");
 
 const app = express();
 
 const PORT = process.env.PORT ?? 3000;
+config(app);
+
+
+app.use("/reg", regRouter);
 
 app.listen(PORT, () => {
   console.log("The server started on", PORT);
