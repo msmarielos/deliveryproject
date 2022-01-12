83394db761367aedc641a7fbc287be9c49b964a1
Ernis <e.badmaev@gmail.com>
2022-01-12 15:27:48 +0300

    settings_hbs

--- config/config.js
+++ config/config.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
 
 const config = (app) => {
-  app.use(morgan("dev"))
+  app.use(morgan("dev"));
+  app.use(express.static(path.join(__dirname, "public")));
+  app.set("view engine", "hbs");
   
-}
+};
+
 
 module.exports = config;
