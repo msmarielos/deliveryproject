cfb94e0b3344ce529766419206312de37f30c99b
Mansur Makhmutov <sir.makhmutov@gmail.com>
2022-01-12 18:15:45 +0300

    Registration delivery

--- public/js/application.js
+++ public/js/application.js
@@ -1,29 +1,43 @@
-document.registrationForm.addEventListener('submit', async (event) => {
-  event.preventDefault();
+document.registrationForm.addEventListener("submit", async(event) => {
+    event.preventDefault();
 
-  const {
-    method,
-    action,
-  } = event.target;
+    const { method, action } = event.target;
 
-  const body = {
-    name: event.target.username.value,
-    email: event.target.email.value,
-    password: event.target.password.value,
-    address: event.target.address.value,
-  };
-  const response = await fetch(action, {
-    method,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(body),
-  });
+    const body = {
+        name: event.target.username.value,
+        email: event.target.email.value,
+        password: event.target.password.value,
+        address: event.target.address.value,
+    };
+    const response = await fetch(action, {
+        method,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    });
 
-  const user = await response.json();
-  console.log(user);
+    const user = await response.json();
+});
 
-  // if (user.isSuccess) {
-  //   alert ('')
-  // }
+document.regDevForm.addEventListener("submit", async(event) => {
+    event.preventDefault();
+
+    const { method, action } = event.target;
+
+    const body = {
+        name: event.target.name.value,
+        email: event.target.email.value,
+        password: event.target.password.value,
+    };
+
+    const response = await fetch(action, {
+        method,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    });
+
+    const delivery = await response.json();
 });
