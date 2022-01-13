e33e4cf11e2b0b22430b38e48f15e67a0cc1169f
Ernis <e.badmaev@gmail.com>
2022-01-13 15:57:05 +0300

    сommentsForCartLogic

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const config = require("./config/config");
 const regRouter = require("./routes/reg.route");
 const regDev = require("./routes/dev.route");
 const mainRouter = require("./routes/main.route");
+const cartRouter = require("./routes/cart.route");
 
 const app = express();
 
@@ -12,6 +13,7 @@ config(app);
 app.use("/reg", regRouter);
 app.use("/deli", regDev);
 app.use("/", mainRouter);
+app.use("/cart", cartRouter);
 
 app.listen(PORT, () => {
   console.log("The server started on", PORT);
--- public/js/cart.js
+++ public/js/cart.js
@@ -1,6 +1,17 @@
-// const allBtn = document.querySelectorAll(".allBtn")
+//нашли все кнопки по селектору
+const addToCart = document.querySelectorAll(".addToCart");
+//на кнопки вешаем слушателей,  в переменную кидаем id блюда(кнопка с аттрибутом-data.id)
+addToCart.forEach((button) => {
+  button.addEventListener("click", async (e) => {
+    e.preventDefault();
 
-// document?.addDishButton.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   const 
-// })
+    const dishId = button.dataset.id;
+    // console.log(dishId);
+    //генерируем путь интерполяцией строк: /cart(корзина) + id выбранного блюда
+    const action = `cart/${dishId}`;
+    //фечом идем по пути указнному для того чтобы передать id блюда на сервак(роут - там вся логика, чтобы обратить к БД)
+    const response = await fetch(action, {
+      method: "POST",
+    });
+  });
+});
