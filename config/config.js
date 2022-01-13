c386ab52a1b2a057efc792b420552b933228a8bd
Mansur Makhmutov <sir.makhmutov@gmail.com>
2022-01-13 16:36:49 +0300

    login

--- config/config.js
+++ config/config.js
@@ -1,15 +1,32 @@
 const express = require("express");
 const morgan = require("morgan");
+const expressSession = require("express-session");
+const FileStore = require("session-file-store")(expressSession);
+const cookieParser = require("cookie-parser");
 // const path = require("path");
-// const bcrypt = require('bcrypt');
+
+const sessionConfig = {
+    store: new FileStore(),
+    name: "user_sid", // Имя куки для хранения id сессии. По умолчанию - connect.sid
+    secret: process.env.SESSION_SECRET ? ? "test", // Секретное слово для шифрования, может быть любым
+    resave: false, // Пересохранять ли куку при каждом запросе
+    saveUninitialized: false, // Создавать ли сессию без инициализации ключей в req.session
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 12, // Срок истечения годности куки в миллисекундах
+        httpOnly: true, // Серверная установка и удаление куки, по умолчанию true
+    },
+};
 
 const config = (app) => {
-  app.use(morgan("dev"));
-  app.use(express.static("public"));
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
+    app.use(morgan("dev"));
+    app.use(express.static("public"));
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(session(sessionConfig))
+    app.use(cookieParser())
 
-  app.set("view engine", "hbs");
+
+    app.set("view engine", "hbs");
 };
 
 
